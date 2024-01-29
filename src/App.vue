63b3c209ce330b72244900b37a539359a0323e93
Pyaephyo33 <pyaephyolinn778899@gmail.com>
2024-01-29 16:20:25 +0630

    ppl

--- src/App.vue
+++ src/App.vue
@@ -90,6 +90,17 @@
   </Disclosure>
   <div class="container mx-auto sm:px-6 lg:px-8">
     <h1 class="font-semibold text-2xl  text-blue-600">Hello World 123 Testing</h1>
+
+
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title">Card title!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
